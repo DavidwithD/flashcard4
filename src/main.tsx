9e4d8abd1fdf15e6d8b0231e36c6e657769085ca
David HL <64745261+DavidwithD@users.noreply.github.com>
2024-01-05 02:16:45 +0800

    add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+function Fallback({ error, resetErrorBoundary }: FallbackProps) {
+  return <p>something went wrong!</p>;
+}
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
